171b32970d97d082a2c0992b326b45f39835c58b
Luis Altuve <luis2ra@gmail.com>
2023-07-16 18:38:10 -0500

    Module 4: Django Admin

--- premios_platzi_app/polls/admin.py
+++ premios_platzi_app/polls/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 
-admin.site.register([Question, Choice])  # se define una lista de los modelos a verse en el Admin de Django
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+# admin.site.register([Question, Choice])  # se define una lista de los modelos a verse en el Admin de Django
+admin.site.register(Question, QuestionAdmin)
